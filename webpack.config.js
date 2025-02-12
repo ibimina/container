b1d814720dcffdd262157e17a39744a62f7ff9f5
ibimina hart <ibimina.c.hart@gmail.com>
2025-02-12 21:05:08 +0100

    remove comment

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,11 @@
 const { ModuleFederationPlugin } = require("webpack").container;
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const URL = process.env.REACT_APP_REMOTE_URL;
-const PORT = process.env.REACT_APP_PORT || 3000;
 const path = require("path");
 
 module.exports = (_, argv) => ({
   entry: "./src/index.js",
-  // mode: "development",
   output: {
-    // publicPath: "auto", // ✅ Ensures correct URL resolution
-    // filename: "[name].[contenthash].js",
-    // clean: true,
+
   path: path.resolve(__dirname, "build"),
     
     publicPath:
